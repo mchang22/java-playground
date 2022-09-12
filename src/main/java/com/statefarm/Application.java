4b02de07eea9d50f1a29b5317b543461fc059620
Michael Chang <michael.chang.vabjsp@statefarm.com>
2022-09-12 13:58:25 -0500

    command-arg

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -2,6 +2,16 @@ package com.statefarm;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        if(args.length == 2){
+            String name = args[0];
+            String email = args[1];
+            System.out.println(name + " <" + email + ">");
+        }
+        if(args.length == 0) {
+            System.out.println("Please specify a name and email.");
+        }else if(args.length == 1){
+            System.out.println("Please specify an email for " +args[0]);
+        }
+
     }
 }
